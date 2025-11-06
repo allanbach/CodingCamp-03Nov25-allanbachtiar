77154df7eed3b6a3768d01d16a57b7675da17a76
allanbach <35921730+allanbach@users.noreply.github.com>
2025-11-07 02:03:30 +0700

    2nd try

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-welcomeMesage()
+// display welcome
+welcomeMessage();
 
-function welcomeMesage() {
-    let name = prompt("Please enter your name:")
-    console.log('Helo ' + name + ', welcome to My Website')
+function welcomeMessage() {
+    // asking for their name
+    let name = prompt("Please enter your name:");
+    if (name !== null) {name = name.charAt(0).toUpperCase() + name.slice(1)
+
+    }
+    
+
+
+    if (name !== null) {
+        // clicked ok without entering name
+        if (name === ""){document.getElementById('greeting').innerHTML = 'Hello, <br/> welcome to <br/> My Website!';}
+        // entered name
+        else {document.getElementById('greeting').innerHTML = 'Hello ' + name + ', <br/> welcome to <br> My Website!';}
+    } else {
+  // clicked Cancel
+  document.getElementById('greeting').innerHTML = 'Hello, welcome to My Website!<br/>Why didnt you tell me your name btw?';}
+
+
 }
+
+// Validate the contact form
+function validateForm() { }
